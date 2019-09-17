af326b3ee3ec29fc574a0091fbfbba423396daf3
asisudai <asisudai@gmail.com>
2019-09-17 00:07:36 -0700

    even better

--- docker_reset.sh
+++ docker_reset.sh
@@ -1,5 +1,6 @@
 # Remove all docker information!
 # Used for development only
 # https://github.com/Radu-Raicea/Dockerized-Flask/wiki/%5BDocker%5D-Remove-all-Docker-volumes-to-delete-the-database
+docker stop `docker ps -a -q`
 docker rm `docker ps -a -q` -f
 docker volume prune
