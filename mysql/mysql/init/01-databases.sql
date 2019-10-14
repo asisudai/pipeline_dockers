b0ad9433951885d3c4b1e047066a6a2955c14849
asisudai <asisudai@gmail.com>
2019-10-13 17:10:11 -0700

    run sql script after startup

---
+++ mysql/mysql/init/01-databases.sql
@@ -0,0 +1,6 @@
+# create unittest databases
+CREATE DATABASE IF NOT EXISTS `unittest`;
+
+# create root user and grant rights
+CREATE USER 'root'@'localhost' IDENTIFIED BY 'local';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
